6082e5afa519f3a6aba072428a0db3be3586fb7e
Paul Butler <paul@driftingin.space>
2025-01-22 15:13:46 -0500

    add example (#15)
    * add example
    
    * fix lockfile
    
    * add check format script
    
    * Clean up example
    
    * Don't put example in workspace
    
    ---------
    
    Co-authored-by: Jake Lazaroff <jake@jamsocket.com>

---
+++ javascript/example/README.md
@@ -0,0 +1,11 @@
+# foreverVM Example
+
+Example of using foreverVM to run Python REPL commands.
+
+You will need an API token (if you need one, reach out to [paul@jamsocket.com](mailto:paul@jamsocket.com)).
+
+Once you have an API token, run this
+
+```sh
+FOREVERVM_TOKEN="******" npm start
+```
---
+++ javascript/example/index.ts
@@ -0,0 +1,27 @@
+import { ForeverVM } from '@forevervm/sdk'
+
+const token = process.env.FOREVERVM_TOKEN
+if (!token) {
+  throw new Error('FOREVERVM_TOKEN is not set')
+}
+
+// Initialize ForeverVM
+const fvm = new ForeverVM(token)
+
+// Connect to a new machine.
+const repl = await fvm.repl(null)
+
+// Execute some code
+let execResult = repl.exec('4 + 4')
+
+// Get the result
+console.log('result:', await execResult.result)
+
+// We can also print stdout and stderr
+execResult = repl.exec('for i in range(10):\n  print(i)')
+
+for await (const output of execResult.output) {
+  console.log(output.stream, output.data)
+}
+
+process.exit(0)
