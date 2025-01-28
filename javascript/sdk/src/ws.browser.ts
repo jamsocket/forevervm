f16dcd586068c9895c948be89f1ebb71f729a487
Jake Lazaroff <1223007+jakelazaroff@users.noreply.github.com>
2025-01-28 12:02:34 -0500

    Use isomorphic-ws (#70)

--- javascript/sdk/src/ws.browser.ts
+++
@@ -1,3 +0,0 @@
-export function websocket(url: string, token: string) {
-  return new WebSocket(url + `?_forevervm_jwt=${token}`)
-}
--- javascript/sdk/src/ws.ts
+++
@@ -1,5 +0,0 @@
-import WebSocket from 'ws'
-
-export function websocket(url: string, token: string) {
-  return new WebSocket(url, { headers: { Authorization: `Bearer ${token}` } })
-}
