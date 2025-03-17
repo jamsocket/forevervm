3b0ea5077abfd7e7529fe4488e51c9dfb5750a6b
Jake Lazaroff <jake@jamsocket.com>
2025-03-17 15:24:07 -0400

    Buffer streamed chunks

--- javascript/sdk/src/index.ts
+++ javascript/sdk/src/index.ts
@@ -55,13 +55,32 @@ export class ForeverVM {
 
     const decoder = new TextDecoderStream()
     const reader = response.body.pipeThrough(decoder).getReader()
+
+    // buffer JSON just in case an object is split across multiple stream chunks
+    let buffer = ''
+
     while (true) {
-      const { done, value = '' } = await reader.read()
+      let { done, value = '' } = await reader.read()
       if (done) return
 
-      const lines = value.split('\n').filter((line) => line.trim())
-      for (const line of lines) {
-        yield JSON.parse(line)
+      // loop until we've read all the data in this chunk
+      while (value) {
+        // find the next newline character
+        const newline = value.indexOf('\n')
+
+        // if there are no more newlines, add the remaining data to the buffer and break
+        if (newline === -1) {
+          buffer += value
+          break
+        }
+
+        // parse and yield the next line from the data
+        const line = value.slice(0, newline)
+        yield JSON.parse(buffer + line)
+
+        // remove the just-processed line from the value and reset the buffer
+        value = value.slice(newline + 1)
+        buffer = ''
       }
     }
   }
@@ -177,6 +196,24 @@ if (import.meta.vitest) {
         i += 1
       }
     }
+  })
+
+  test('execResultStream with image', async () => {
+    const fvm = new ForeverVM({ token: FOREVERVM_TOKEN, baseUrl: FOREVERVM_API_BASE })
+    const { machine_name } = await fvm.createMachine()
+
+    const code = `import matplotlib.pyplot as plt
+plt.plot([0, 1, 2], [0, 1, 2])
+plt.title('Simple Plot')
+plt.show()`
+
+    const { instruction_seq } = await fvm.exec(code, machine_name)
+    expect(instruction_seq).toBe(0)
+
+    for await (const _ of fvm.execResultStream(machine_name, instruction_seq as number)) {
+    }
+
+    // if we reach this point, it means all the stream chunks were valid JSON
   })
 
   test('createMachine with tags', async () => {
